f48ec943b0b244a85d3f8120b33b930411f87f40
TzeMingHo <tzemingho@gmail.com>
2026-05-06 17:58:45 +0100

    added logic for handling posting message and update display

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -28,15 +28,65 @@ async function fetchChatHistory() {
 
 async function chatDisplay() {
   const chatDisplayArea = document.getElementById("chat-display-area");
+  chatDisplayArea.innerHTML = '';
   const chatHistoryArray = await fetchChatHistory();
   if (!chatHistoryArray) {
     chatDisplayArea.append(createEmptyMessage());
   } else {
     chatDisplayArea.append(...createMessageThreads(chatHistoryArray));
   }
-  console.log(chatHistoryArray);
+}
+
+async function postingMessage(messageString, userString) {
+  try {
+    const response = await fetch("http://localhost:4000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: messageString,
+        user: userString
+      })
+    })
+    if (response.ok) {
+      const confirmMessage = await response.text();
+      if (confirmMessage == "sent") {
+        chatDisplay()
+      }
+    }
+  } catch (error) {
+    console.error(`Failed to post message: ${error}`)
+  }
+}
+
+async function messageSubmitHandler(e, messageString, userString) {
+  e.preventDefault();
+  if (!messageString || !userString) {
+    console.error(`Message or user cannot be empty.`)
+    return;
+  } else {
+    await postingMessage(messageString, userString)
+  }
+}
+
+function messageInputHandler() {
+  let messageString; 
+  document.getElementById("message-input").addEventListener("input", (e) => {
+    messageString = e.target.value.trim();
+  })
+
+  let userString;
+  document.getElementById("user-name-input").addEventListener("input", (e) => {
+    userString = e.target.value.trim();
+  })
+
+  document.getElementById("message-submit-button").addEventListener("click", (e) => {
+    messageSubmitHandler(e, messageString, userString)
+  })
 }
 
 window.onload = async () => {
   await chatDisplay();
+  messageInputHandler();
 };
